32fb914a2520e52a847c22642c5ed93503668d60
Prvn369 <166159938+Prvn369@users.noreply.github.com>
2024-04-16 15:08:56 +0530

    Update first.js

--- first.js
+++ first.js
@@ -7,7 +7,6 @@ setInterval(() => {
     let m = date.getMinutes();
     let s = date.getSeconds();
 
-    time.children[0].innerText = h;
     //  For Hours 01 Format 
 
     if (h == 0 || h == 1 || h == 2 || h == 3 || h == 4 || h == 5 || h == 6 || h == 7 || h == 8 || h == 9) {
@@ -15,6 +14,8 @@ setInterval(() => {
     } else {
         time.children[0].innerText = h
     }
+
+    
     //  For Minutes 01 Format 
 
     if (m == 0 || m == 1 || m == 2 || m == 3 || m == 4 || m == 5 || m == 6 || m == 7 || m == 8 || m == 9) {
@@ -22,6 +23,8 @@ setInterval(() => {
     } else {
         time.children[2].innerText = m;
     }
+
+    
     //  For Seconds 01 Format 
 
     if (s == 0 || s == 1 || s == 2 || s == 3 || s == 4 || s == 5 || s == 6 || s == 7 || s == 8 || s == 9) {
